e6eecf11c40f6f6fd6a6554d8d5c3ca4f784deef
ZacharyLloyd <46659989+ZacharyLloyd@users.noreply.github.com>
2019-04-01 13:13:20 -0700

    Finished project

--- References_Project/References_Project.cpp
+++ References_Project/References_Project.cpp
@@ -1,44 +1,55 @@
-// Swap
-// Demonstrates passing references to alter argument variables
+//Swapping Bank Funds References Project
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void normalTransaction(int x, int y);
-void hackedTransaction(int& x, int& y);
+void originalBalance(int x, int y); // Declaring orginalBalance function
+void normalTransaction(int x, int y); // Declaring noramlTransaction function
+void hackedTransaction(int& x, int& y); // Declaring hackedTransaction function with references
+int normalBalance = 15000; // Declaring starting balance variable
+int hackedBalance = 100; // Declaring hacked balance variable
 
 int main()
 {
-	int myScore = 150;
-	int yourScore = 1000;
-	cout << "Original values\n";
-	cout << "myScore: " << myScore << "\n";
-	cout << "yourScore: " << yourScore << "\n\n";
 
-	cout << "Calling hackedTransaction()\n";
-	hackedTransaction(myScore, yourScore);
-	cout << "myScore: " << myScore << "\n";
-	cout << "yourScore: " << yourScore << "\n\n";
+	originalBalance(normalBalance, hackedBalance); //Calling orignalBalance function
+	normalTransaction(normalBalance, hackedBalance); //Calling noramlTransaction function
+	hackedTransaction(normalBalance, hackedBalance); //Calling hackedTransaction function
 
-	cout << "Calling normalTransaction()\n";
-	normalTransaction(myScore, yourScore);
-	cout << "myScore: " << myScore << "\n";
-	cout << "yourScore: " << yourScore << "\n";
-
+	system("pause");
 	return 0;
 }
-
-void hackedTransaction(int x, int y)
+//Creating originalBalance function
+void originalBalance(int x, int y) 
 {
-	int temp = x;
-	x = y;
-	y = temp;
+	//Displaying orginalBalance function
+	cout << "Original values\n";
+	cout << "normalBalance: " << normalBalance << "\n";
+	cout << "hackedBalance: " << hackedBalance << "\n\n";
 }
-
-void normalTransaction(int& x, int& y)
+//Creating noramlTransaction function
+void normalTransaction(int x, int y)
 {
+	//Swapping the parameters
 	int temp = x;
 	x = y;
 	y = temp;
+	//Displaying normalTransaction function
+	cout << "Calling normalTransaction()\n";
+	cout << "normalBalance: " << normalBalance << "\n";
+	cout << "hackedBalance: " << hackedBalance << "\n";
+}
+//Creating hackedTransaction function
+void hackedTransaction(int& x, int& y)
+{
+	//Swapping the parameters
+	int temp = x;
+	x = y;
+	y = temp;
+	//Displaying hackedTransaction function
+	cout << "Calling hackedTransaction()\n";
+	cout << "normalBalance: " << normalBalance << "\n";
+	cout << "hackedBalance: " << hackedBalance << "\n\n";
 }
